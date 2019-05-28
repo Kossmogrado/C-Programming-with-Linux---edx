f637258e4835a1397865025a1399ee6139f54a7e
Kossmogrado <51132385+Kossmogrado@users.noreply.github.com>
2019-05-28 14:52:50 -0300

    Add files via upload

---
+++ 1.1/1.1.6 Activity use a for-loop to print a line multiple times.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int l=0;
+    for(l=0;l<3;l++)
+    {
+        printf("C is Fun!\n");
+    }
+    return 0;
+}
---
+++ 1.1/1.1.7 Activity use multiple for-loops.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int l=0;
+    for (l=0;l<3;l++)
+    {
+        printf("C is fun!\n");
+    }
+    printf("\n");
+    for (l=0;l<5;l++)
+    {
+        printf("\nwe can do everything with it!");
+    }
+    return 0;
+}
